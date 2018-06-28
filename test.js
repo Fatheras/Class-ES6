1e3022cfa46fc6c488b09a8fc034e920b63e73cb
Fatheras <kosras.96@gmail.com>
2018-06-28 23:01:27 +0300

    refactoring

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-function RenderTest() {
+function Test() {
   element = document.getElementById('testDiv');
   let imagesArray = [];
   imagesArray[0] = "https://dota2.ru/img/uploads/18/06/1050555.jpg"
@@ -6,12 +6,16 @@ function RenderTest() {
   imagesArray[2] = "https://dota2.ru/img/uploads/18/06/1050555.jpg"
   imagesArray[3] = "https://dota2.ru/img/uploads/18/06/1050555.jpg"
   imagesArray[4] = "https://dota2.ru/img/uploads/18/06/1050555.jpg"
+  console.log("Length of the array before sending it to imageRenderer: " + imagesArray.length);
+  let imageRenderer = new ImageRenderer(imagesArray);
 
-  let visual = new ImageRenderer(arrayWithImages);
-  visual.add("https://dota2.ru/img/uploads/18/06/1050555.jpg");
-  visual.remove("https://dota2.ru/img/uploads/18/06/1050555.jpg");
-  visual.remove("https://dota2.ru/img/uploads/18/06/1050555.jpg");
-  visual.render(element);
+  imageRenderer.add("https://dota2.ru/img/uploads/18/06/1050555.jpg");
+  console.log("One element was added to the array. The current number of elements in the array: " + imagesArray.length);
+  imageRenderer.remove("https://dota2.ru/img/uploads/18/06/1050555.jpg");
+  imageRenderer.remove("https://dota2.ru/img/uploads/18/06/1050555.jpg");
+  console.log("Two elements of the array were removed. The current number of elements in the array: " + imagesArray.length);
+  imageRenderer.render(element);
+  console.log("All images are displayed on the page. Number of images on the page: " + imagesArray.length);
 }
 
-GetElement();
+Test();
